293403e74b4d79e8db971f667a0d6ce57a24f266
Rovii24 <rovipark24@gmail.com>
2024-04-16 13:25:57 -0700

    Collective commit fot python IO projects

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
-"""module for addng an item"""
+"""
+Script that adds all arguments to a Python list, and then saves them to a file
+"""
 
-
-import json
-import os.path
-import sys
-
-
+from sys import argv
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-my = "add_item.json"
-json = []
+filename = "add_item.json"
 
+try:
+    json_list = load_from_json_file(filename)
+except FileNotFoundError:
+    json_list = []
 
-if os.path.exists(my):
-    json = load_from_json_file(my)
+for arg in argv[1:]:
+    json_list.append(arg)
 
-for arg in range(1, len(sys.argv)):
-    json.append(sys.argv[arg])
-
-save_to_json_file(json, my)
+save_to_json_file(json_list, filename)
