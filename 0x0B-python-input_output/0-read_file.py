ee901dd244393fcca83da602ec015b660367eab0
Rovii24 <rovipark24@gmail.com>
2024-04-16 13:06:41 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
 """
-A module containing IO functions for file.
+Contains the read_file function
 """
 
+
 def read_file(filename=""):
-    """
-    Reads a UTF-8 encoded text file and prints it to stdout.
-
-
-    Args
-         filename (str): The name of the file to read.
-    """
-    with open(filename, encoding='utf-8') as f:
-        print(f.read(), end='')
+    """""reads a text file(UTF8) and prints it to stdout"""
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
